5ddfbd3590e04288df0d3da1e727cade780244d3
fmelendezherize <fmelendezherize@gmail.com>
2014-11-20 17:37:53 -0430

    untracked

---
+++ CoreApplication/Model/Dtos/UsuarioDTO.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+namespace Decktra.PubliPuntoEstacion.CoreApplication.Model.Dtos
+{
+    public class UsuarioDTO
+    {
+        public string Cedula { get; set; }
+        public string Codigo { get; set; }
+        public string Email { get; set; }
+        public string Pin { get; set; }
+    }
+
+    public class ListOfUsuario
+    {
+        public List<UsuarioDTO> Usuarios { get; set; }
+    }
+}
